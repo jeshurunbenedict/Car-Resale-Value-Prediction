acaed2693eae737a6fc9040de4eec31f08006c4c
Jeshurun Benedict <55593610+jeshurunbenedict@users.noreply.github.com>
2022-12-04 14:56:49 +0530

    Add files via upload

---
+++ Flask/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+import numpy as np
+import pickle
+
+app = Flask(__name__)
+
+def ValuePredictor(to_predict_list):
+    to_predict = np.array(to_predict_list)
+    loaded_model = pickle.load(open("Car Resale Value Prediction.pkl", "rb"))
+    result = loaded_model.predict([to_predict])
+    return result[0]
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/login', methods =['POST'])
+def login():
+    if request.method == 'POST':
+        to_predict_list = request.form.to_dict()
+        to_predict_list = list(to_predict_list.values())
+        to_predict_list = list(map(int, to_predict_list))
+        result = ValuePredictor(to_predict_list)
+        return render_template("index.html",y = "The Value of the Car is :   " + str(result) )
+
+if __name__ == '__main__' :
+    app.run(debug= True)
